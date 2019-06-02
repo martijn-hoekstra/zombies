ae639f10a915137434339c89f780206e59ef5b64
Martijn Hoekstra <martijn_129@icloud.com>
2019-06-02 12:09:50 +0200

    Added templating engine and two views

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use('/', gameRoutes);
+app.use(gameRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).render('404');
 });
 
 app.listen(3000);
--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const survivors = [
+    {name: 'Marty', activity: 'Repairing safehouse'},
+    {name: 'Josh', activity: 'Exploring north'},
+    {name: 'Lucia', activity: 'Cooking'}
+];
+
 router.get('/survivors', (req, res, next) => {
     res.send(`<h1>Survivors</h1>`);
 });
@@ -21,8 +27,8 @@ router.get('/explore', (req, res, next) => {
     res.send(`<h1>Explore</h1>`);
 });
 
-router.use('/', (req, res, next) => {
-    res.send(`<h1>Home</h1>`);
+router.use('/dashboard', (req, res, next) => {
+    res.render('index', {survivors: survivors});
 });
 
 module.exports = router;
